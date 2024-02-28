24b10af7a6aa5d2b3d9b626b292769ff5b208cae
liangtianyu <124244236@qq.com>
2024-02-28 09:16:38 +0800

    namespace合并配置信息

--- src/console/v1/namespace.rs
+++ src/console/v1/namespace.rs
@@ -7,7 +7,8 @@ use actix_web::{get, web};
 
 #[get("")]
 pub async fn get_namespaces(data: web::Data<AppState>) -> web::Json<RestResult<Vec<Namespace>>> {
-    let namespaces: Vec<Namespace> = NamespaceService::find_all(data.conns.get(0).unwrap()).await;
+    let namespaces: Vec<Namespace> =
+        NamespaceService::NamespaceOperationService::find_all(data.conns.get(0).unwrap()).await;
     let rest_result = RestResult::<Vec<Namespace>>::success(namespaces);
 
     web::Json(rest_result)
